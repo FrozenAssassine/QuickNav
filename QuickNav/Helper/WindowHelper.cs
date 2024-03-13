b2ffd181c8f684a827ddede98ba39ad4fc18a594
Julius Kirsch <Julius@skirsch.de>
2024-03-13 10:48:52 +0100

    Projektdateien hinzufügen.

---
+++ QuickNav/Helper/WindowHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.UI.Windowing;
+using System.Threading.Tasks;
+using Windows.Devices.Display;
+using Windows.Devices.Enumeration;
+
+namespace QuickNav.Helper
+{
+    internal class WindowHelper
+    {
+        public static async Task CenterWindow(AppWindow window)
+        {
+
+            var displayList = await DeviceInformation.FindAllAsync
+                     (DisplayMonitor.GetDeviceSelector());
+
+            var monitorInfo = await DisplayMonitor.FromInterfaceIdAsync(displayList[0].Id);
+
+            var screenHeight = monitorInfo.NativeResolutionInRawPixels.Height;
+            var screenWidth = monitorInfo.NativeResolutionInRawPixels.Width;
+
+            var width = Math.Clamp(screenWidth / 4, 400, 800);
+            var height = Math.Clamp(screenHeight / 4, 200, 600);
+
+            var windowSize = new Windows.Graphics.RectInt32(screenWidth / 2 - (width / 2), (screenHeight / 2 - (height / 2)), width, height);
+            window.MoveAndResize(windowSize);
+        }
+    }
+}
---
+++ QuickNav/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using QuickNav.Helper;
+using System;
+using System.Threading.Tasks;
+using Windows.Devices.Display;
+using Windows.Devices.Enumeration;
+using WinRT.Interop;
+
+namespace QuickNav
+{
+    public sealed partial class MainWindow : Window
+    {
+        private AppWindow m_AppWindow;
+        private OverlappedPresenter? _presenter;
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            m_AppWindow = GetAppWindowForCurrentWindow();
+
+            this.Activated += MainWindow_Activated;
+
+            _presenter = m_AppWindow.Presenter as OverlappedPresenter;
+        }
+
+        private AppWindow GetAppWindowForCurrentWindow()
+        {
+            IntPtr hWnd = WindowNative.GetWindowHandle(this);
+            WindowId wndId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            return AppWindow.GetFromWindowId(wndId);
+        }
+
+        private async void MainWindow_Activated(object sender, WindowActivatedEventArgs args)
+        {
+            if (_presenter is null)
+            {
+                return;
+            }
+
+            await WindowHelper.CenterWindow(this.m_AppWindow);
+
+            _presenter.SetBorderAndTitleBar(hasBorder: false, hasTitleBar: false);
+            _presenter.IsAlwaysOnTop = true;
+            _presenter.IsResizable = false;
+        }
+    }
+}
