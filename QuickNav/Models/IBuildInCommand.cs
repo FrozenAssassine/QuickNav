9caf413a91ba237428111192e202c6ba67ce23ea
Finn Freitag <finn-freitag@web.de>
2024-03-16 11:18:04 +0100

    feat: IBuildInCommand

---
+++ QuickNav/Models/IBuildInCommand.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using QuickNavPlugin;
+using QuickNavPlugin.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickNav.Models
+{
+    internal interface IBuildInCommand : ICommand
+    {
+        bool RunCommand(string parameters, out Page content);
+    }
+}
