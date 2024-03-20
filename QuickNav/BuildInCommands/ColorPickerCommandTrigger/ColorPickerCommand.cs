d80d7de385cc4849f758b6e880d7d0217b0a2bd5
Finn Freitag <finn-freitag@web.de>
2024-03-20 10:00:42 +0100

    feat: color picker command #3

---
+++ QuickNav/BuildInCommands/ColorPickerCommandTrigger/ColorPickerCommand.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using QuickNav.Models;
+using QuickNavPlugin;
+using QuickNavPlugin.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickNav.BuildInCommands.ColorPickerCommandTrigger
+{
+    public class ColorPickerCommand : ITriggerCommand, IBuildInCommand
+    {
+        public string CommandTrigger => "";
+
+        public string[] Keywords => new string[] { "color", "picker" };
+
+        public string Description => "Pick a color from color circle.";
+
+        public Uri Icon => new Uri("ms-appx://App/Assets/commands/colorpicker.png");
+
+        public Priority Priority => Priority.Low;
+
+        public string Name(string query)
+        {
+            return "Color Picker";
+        }
+
+        public bool RunCommand(string parameters, out ContentElement content)
+        {
+            content = null;
+            return false;
+        }
+
+        public bool RunCommand(string parameters, out Page content)
+        {
+            content = new QuickNav.Views.ColorPicker();
+            return true;
+        }
+    }
+}
