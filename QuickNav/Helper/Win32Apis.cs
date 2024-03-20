5a0a39b2d9487f715bebf9faa7f048508f6a3fcb
Finn Freitag <finn-freitag@web.de>
2024-03-20 20:46:54 +0100

    feat: improvements for lockscreen command

--- QuickNav/Helper/Win32Apis.cs
+++ QuickNav/Helper/Win32Apis.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 
 namespace QuickNav.Helper;
 
@@ -7,4 +8,10 @@ internal class Win32Apis
     //Lock your device:
     [DllImport("user32.dll")]
     public static extern bool LockWorkStation();
+
+    [DllImport("user32.dll")]
+    public static extern IntPtr SendMessage(IntPtr hwnd, uint msg, int wParam, int lParam);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
 }
