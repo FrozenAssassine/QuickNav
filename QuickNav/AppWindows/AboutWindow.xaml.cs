8818bbea5e90579b0b80cc7bbb128b0486b56557
Julius Kirsch <Julius@skirsch.de>
2024-03-21 14:28:33 +0100

    feat: about page

--- QuickNav/AppWindows/AboutWindow.xaml.cs
+++ QuickNav/AppWindows/AboutWindow.xaml.cs
@@ -1,16 +1,64 @@
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Documents;
+using QuickNav.Helper;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Windows.Navigation;
 using Windows.ApplicationModel;
 
 namespace QuickNav.AppWindows;
 
 public sealed partial class AboutWindow : Window
 {
+
+    public string AppVersion => AppVersionHelper.GetAppVersion();
+    public string DeveloperName => Package.Current.PublisherDisplayName;
     public AboutWindow()
     {
         this.InitializeComponent();
 
         this.AppWindow.SetIcon(Path.Combine(Package.Current.InstalledLocation.Path, "Assets\\AppIcon\\appicon.ico"));
+    }
 
+    private async void NavigateToLink_Click(Controls.SettingsControl sender)
+    {
+        if (sender.Tag == null)
+            return;
+
+        await Windows.System.Launcher.LaunchUriAsync(new Uri(sender.Tag.ToString()));
+    }
+
+    private void SetChangelog()
+    {
+        //Simple parser to make headlines bigger and add paragraphs
+        string filePath = Path.Combine(Package.Current.InstalledLocation.Path, "Assets", "changelog.txt");
+        var data = File.ReadAllLines(filePath);
+        List<Paragraph> paragraphs = new List<Paragraph> { new Paragraph() };
+        for (int i = 0; i < data.Length; i++)
+        {
+            string currentLine = data[i];
+            Run line = new Run();
+
+            //Headline:
+            if (currentLine.StartsWith("#"))
+            {
+                currentLine = currentLine.Remove(0, 1);
+                line.FontSize = 24;
+            }
+            //Paragraph
+            else if (currentLine.StartsWith("---"))
+            {
+                currentLine = currentLine.Remove(0, 3);
+                paragraphs.Add(new Paragraph());
+            }
+
+            line.Text = currentLine + "\n";
+            paragraphs[paragraphs.Count - 1].Inlines.Add(line);
+        }
+        foreach (var paragraph in paragraphs)
+        {
+            ChangelogDisplay.Blocks.Add(paragraph);
+        }
     }
 }
---
+++ QuickNav/Helper/AppVersionHelper.cs
@@ -0,0 +1,14 @@
+﻿using Windows.ApplicationModel;
+
+namespace QuickNav.Helper
+{
+    internal class AppVersionHelper
+    {
+        public static string GetAppVersion()
+        {
+            return Package.Current.Id.Version.Major + "." +
+                Package.Current.Id.Version.Minor + "." +
+                Package.Current.Id.Version.Build;
+        }
+    }
+}
