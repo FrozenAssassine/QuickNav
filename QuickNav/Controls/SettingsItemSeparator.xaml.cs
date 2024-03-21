caea1f17e4ca278ca71b053b4c7daf37e9c0dcca
Julius Kirsch <Julius@skirsch.de>
2024-03-21 14:19:55 +0100

    feat: settings controls

---
+++ QuickNav/Controls/SettingsItemSeparator.xaml.cs
@@ -0,0 +1,16 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace QuickNav.Controls
+{
+    public sealed partial class SettingsItemSeparator : UserControl
+    {
+        public SettingsItemSeparator()
+        {
+            this.InitializeComponent();
+        }
+
+        public new object Content { get; set; }
+
+        public string Header { get; set; }
+    }
+}
