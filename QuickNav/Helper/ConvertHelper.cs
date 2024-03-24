38d45fa94a7ee6f8c2ce706650f8b5ef8fb33014
Julius Kirsch <Julius@skirsch.de>
2024-03-24 13:53:42 +0100

    feat: added AppSettings

--- QuickNav/Helper/ConvertHelper.cs
+++ QuickNav/Helper/ConvertHelper.cs
@@ -9,4 +9,23 @@ internal class ConvertHelper
     {
         return visible ? Visibility.Visible : Visibility.Collapsed;
     }
+    public static int ToInt(object value, int defaultValue = 0)
+    {
+        if (value != null)
+        {
+            if (int.TryParse(value.ToString(), out int converted) == true)
+                return converted;
+        }
+        return defaultValue;
+    }
+
+    public static bool ToBoolean(object value, bool defaultValue = false)
+    {
+        if (value != null)
+        {
+            if (bool.TryParse(value.ToString(), out bool converted) == true)
+                return converted;
+        }
+        return defaultValue;
+    }
 }
