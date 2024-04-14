eeb14d4e144bf2187c8b97b44d69d9b6258e70db
Finn Freitag <finn-freitag@web.de>
2024-04-14 14:13:36 +0200

    feat: image converter webp and heic support

--- QuickNav/BuildInCommands/ConverterCommand/ImageConverter.cs
+++ QuickNav/BuildInCommands/ConverterCommand/ImageConverter.cs
@@ -1,4 +1,6 @@
-﻿using QuickNav.Helper;
+﻿using CommunityToolkit.WinUI.UI.Controls;
+using ImageMagick;
+using QuickNav.Helper;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -8,15 +10,14 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
-using static System.Windows.Forms.DataFormats;
 
 namespace QuickNav.BuildInCommands.ConverterCommand
 {
     public class ImageConverter : IConverter
     {
-        public string[] InputTypes => new string[] { "bmp", "png", "jpg", "jpeg", "ico" };
+        public string[] InputTypes => new string[] { "bmp", "png", "jpg", "jpeg", "ico", "gif", "webp", "heic" };
 
-        public string[] OutputTypes => new string[] { "bmp", "png", "jpg", "jpeg", "gif", "ico" };
+        public string[] OutputTypes => new string[] { "bmp", "png", "jpg", "jpeg", "ico", "gif" };
 
         public string[] ConvertTo(string[] paths, string OutputType)
         {
@@ -32,9 +33,26 @@ namespace QuickNav.BuildInCommands.ConverterCommand
                 for (int i = 0; i < paths.Length; i++)
                 {
                     string p = Path.Combine(folder, Path.GetFileNameWithoutExtension(paths[i]) + "." + OutputType);
+                    string ext = Path.GetExtension(paths[i]).ToLower();
                     if (Path.GetExtension(paths[i]).ToLower() == ".ico")
                     {
                         File.Copy(paths[i], p);
+                    }
+                    else if (ext == ".webp" || ext == ".heic")
+                    {
+                        var magickImages = new MagickImageCollection(paths[i]);
+                        var ms = new MemoryStream();
+                        if (magickImages.Count > 1)
+                        {
+                            magickImages[0].Write(ms, MagickFormat.Bmp);
+                        }
+                        else
+                        {
+                            magickImages.Write(ms, MagickFormat.Bmp);
+                        }
+                        Bitmap bmp = new Bitmap(ms);
+                        bmp.Tag = ms; // keep MemoryStream from being garbage collected while Bitmap is in use
+                        File.WriteAllBytes(p, ConvertHelper.BitmapToIcon(bmp));
                     }
                     else
                     {
@@ -69,8 +87,24 @@ namespace QuickNav.BuildInCommands.ConverterCommand
                 for(int i = 0; i < paths.Length; i++)
                 {
                     Bitmap bmp;
-                    if (Path.GetExtension(paths[i]).ToLower() == ".ico")
+                    string ext = Path.GetExtension(paths[i]).ToLower();
+                    if (ext == ".ico")
                         bmp = new Icon(paths[i]).ToBitmap();
+                    else if (ext == ".webp" || ext == ".heic" || ext == ".gif")
+                    {
+                        var magickImages = new MagickImageCollection(paths[i]);
+                        var ms = new MemoryStream();
+                        if (magickImages.Count > 1)
+                        {
+                            magickImages[0].Write(ms, MagickFormat.Bmp);
+                        }
+                        else
+                        {
+                            magickImages.Write(ms, MagickFormat.Bmp);
+                        }
+                        bmp = new Bitmap(ms);
+                        bmp.Tag = ms; // keep MemoryStream from being garbage collected while Bitmap is in use
+                    }
                     else
                         bmp = new Bitmap(paths[i]);
                     string p = Path.Combine(folder, Path.GetFileNameWithoutExtension(paths[i]) + "." + OutputType);
